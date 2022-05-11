6b16cd69629114261cff1fde8de543a32188387d
estherwee <estherweexueli@gmail.com>
2022-05-11 19:09:58 +0800

    update

--- snake_gridVer.js
+++ snake_gridVer.js
@@ -1,7 +1,3 @@
-//? to consider:
-// adding "blocks"/"walls" to avoid inside arena once past a certain score
-// cannot hit own tail
-// choose speed difficulty at the start
 
 const data = {
     arena: {
@@ -19,10 +15,106 @@ const data = {
         row: null,
         cell: null,
     },
-    speed: null
+    speed: null,
+    displaySpeed: ""
 }
 
 let gameOn;
+
+const endGame = () => {
+    $(".arena").remove()
+    $(".points").remove()
+    endingPage();
+}
+
+const endingPage = () => {
+    const $end = $("<div>").addClass("end");
+    const $lose = $("<div>").attr("id", "lose").text("GAME OVER")
+    const $again = $("<div>").attr("id", "again").text("Replay")
+    const $score = $("<div>").attr("id", "score").text(`You fed the snake ${data.foodCount} times in ${data.displaySpeed} mode`)
+    const $returnHome = $("<div>").attr("id", "returnHome").text("Click to re-select difficulty")
+    $lose.append($score, $again, $returnHome)
+    $end.append($lose)
+    $(".container").append($end)
+
+    $again.on("click", () => {
+        
+        $(".end").remove();
+        data.snake = [
+            {row: 1, cell: 4},
+            {row: 1, cell: 3},
+            {row: 1, cell: 2},
+            {row: 1, cell: 1}
+        ];
+        data.direction = "right";
+        data.foodCount = 0;
+        initGame(data.speed);
+    })
+
+    $returnHome.on("click", () => {
+        location.reload()
+    })
+}
+
+const checkOutOfBoundary = () => {
+    const rightWallCell = data.arena.boxes - 1;
+    const leftWallCell = 0;
+    const topWallRow = 0;
+    const bottomWallRow = data.arena.boxes -1;
+    if (data.snake[0].cell > rightWallCell
+        || data.snake[0].cell < leftWallCell
+        || data.snake[0].row < topWallRow
+        || data.snake[0].row > bottomWallRow) {
+
+        clearInterval(gameOn)
+        endGame(); 
+    } else {
+        return false;
+    }
+}  
+
+const checkHitOwnTail = () => {
+    if ($(".snakeHead").hasClass("snakeBody")) {
+        clearInterval(gameOn)
+        endGame(); 
+    } 
+}
+
+const makeFood = () => {
+    const row = Math.floor(Math.random() * data.arena.boxes);
+    const cell = Math.floor(Math.random() * data.arena.boxes);
+
+    while ($(`#row${row}cell${cell}`).hasClass("snakeBody") || $(`#row${row}cell${cell}`).hasClass("snakeHead")) {
+        row = Math.floor(Math.random() * data.arena.boxes);
+        cell = Math.floor(Math.random() * data.arena.boxes);
+    }    
+    const $foodCell = $(`#row${row}cell${cell}`)
+    
+    $foodCell.addClass("food");
+    data.food.row = row;
+    data.food.cell = cell;
+
+    const red = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    $(".food").css("background-color", `rgb(${red}, ${green}, ${blue})`).css("border-color", "white");
+}
+
+const checkFood = () => {
+    if (data.snake[0].row === data.food.row && data.snake[0].cell === data.food.cell) {
+        $(".food").removeClass("food").css("background-color","").css("border-color", "");
+        data.foodCount++;
+        updatePoints();
+        makeFood();
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const updatePoints = () => {
+    $(".points").text(`SCORE: ${data.foodCount}`);
+}
 
 $(document).on("keydown", (event) => {
     if (event.key === "ArrowDown" || event.key === "Down" || event.key === "s" || event.key === "S") { 
@@ -52,75 +144,6 @@ $(document).on("keydown", (event) => {
     }
 })
 
-
-const endGame = () => {
-    $(".table").remove()
-    $(".points").remove()
-    endingPage();
-}
-
-const endingPage = () => {
-    const $end = $("<div>").addClass("end");
-    const $lose = $("<div>").attr("id", "lose").text("GAME OVER")
-    const $again = $("<div>").attr("id", "again").text("Replay")
-    const $score = $("<div>").attr("id", "score").text(`You fed the snake ${data.foodCount} times`)
-    const $returnHome = $("<div>").attr("id", "returnHome").text("Click to re-select difficulty")
-    $lose.append($score, $again, $returnHome)
-    $end.append($lose)
-    $(".container").append($end)
-
-    $again.on("click", () => {
-        // $(".container").empty()
-        $(".end").remove();
-        data.snake = [
-            {row: 1, cell: 4},
-            {row: 1, cell: 3},
-            {row: 1, cell: 2},
-            {row: 1, cell: 1}
-        ];
-        data.direction = "right";
-        data.foodCount = 0;
-        initGame(data.speed);
-    })
-
-    $returnHome.on("click", () => {
-        location.reload()
-    })
-}
-
-const checkOutOfBoundary = () => {
-    const rightWallCell = data.arena.boxes - 1;
-    const leftWallCell = 0;
-    const topWallRow = 0;
-    const bottomWallRow = data.arena.boxes - 1;
-    if (data.snake[0].cell > rightWallCell
-        || data.snake[0].cell < leftWallCell
-        || data.snake[0].row < topWallRow
-        || data.snake[0].row > bottomWallRow) {
-
-        clearInterval(gameOn)
-        endGame(); 
-    } else {
-        return false;
-    }
-}   
-
-const updatePoints = () => {
-    $(".points").text(`SCORE: ${data.foodCount}`);
-}
-
-const checkFood = () => {
-    if (data.snake[0].row === data.food.row && data.snake[0].cell === data.food.cell) {
-        $(".food").removeClass("food").css("background-color","").css("border-color", "");
-        data.foodCount++;
-        updatePoints();
-        makeFood();
-        return true;
-    } else {
-        return false;
-    }
-}
-
 const updateSnake = () => {
     //check food
     if (!checkFood()) {
@@ -143,81 +166,53 @@ const updateSnake = () => {
     
 }
 
-const checkHitOwnTail = () => {
-    if ($(".snakeHead").hasClass("snakeBody")) {
-        clearInterval(gameOn)
-        endGame(); 
-    } 
-}
-
 const showSnake = () => {
-        updateSnake();
-        checkOutOfBoundary();
-        // clear snake
-        $("td").removeClass("snakeBody").removeClass("snakeHead");
-        // print snake
-        for (let i = 0; i < data.snake.length; i++) {
-            const $snakeID = $(`#row${data.snake[i].row}cell${data.snake[i].cell}`);
-            if (i === 0) {
-                $snakeID.addClass("snakeHead");
-            } else {
-                $snakeID.addClass("snakeBody")
-            }
+    updateSnake();
+    checkOutOfBoundary();
+    // clear snake
+    $(".snakeBody").removeClass("snakeBody")
+    $(".snakeHead").removeClass("snakeHead");
+    // print snake
+    for (let i = 0; i < data.snake.length; i++) {
+        const $snakeID = $(`#row${data.snake[i].row}cell${data.snake[i].cell}`);
+        if (i === 0) {
+            $snakeID.addClass("snakeHead");
+        } else {
+            $snakeID.addClass("snakeBody")
         }
-       
-        checkHitOwnTail();
-}
-
-const makeFood = () => {
-    const row = Math.floor(Math.random() * data.arena.boxes);
-    const cell = Math.floor(Math.random() * data.arena.boxes);
-
-    const $foodCell = $(`#row${row}cell${cell}`)
-    if ($foodCell.hasClass("snakeBody") || $foodCell.hasClass("snakeHead")) {
-        return makeFood();
-    } else {
-        $foodCell.addClass("food");
-        data.food.row = row;
-        data.food.cell = cell;
-
-        const red = Math.floor(Math.random() * 256);
-        const blue = Math.floor(Math.random() * 256);
-        const green = Math.floor(Math.random() * 256);
-        $(".food").css("background-color", `rgb(${red}, ${green}, ${blue})`).css("border-color", "white");
     }
+    checkHitOwnTail();
 }
 
-
-// const makeFood = () => {
+const initGame = (speed) => {
+    makePoints();
+    makeTable();
+    // showSnake();
+    // makeFood();
     
-//     data.food.row = Math.floor(Math.random() * data.arena.boxes);
-//     data.food.cell = Math.floor(Math.random() * data.arena.boxes);
-    
-//     for (let i = 0; i < data.snake.length; i++) {
-//         if (data.snake[i].row === data.food.row && data.snake[i].cell === data.food.cell) {
-//             makeFood();
-//         } else {
-//             const $foodID = $(`#row${data.food.row}cell${data.food.cell}`)
-//             $foodID.addClass("food");
-//         }
-//     }
-// }
-
+    gameOn = setInterval(showSnake, speed);   
+    makeFood();
+}
 
 const makeTable = () => {
-    const $div = $("<div>").addClass("table")
-    const $table = $("<table>").addClass("arena")
+    const $table = $("<div>").addClass("table")
+    const $arena = $("<div>").addClass("arena")
     for (let i = 0; i < data.arena.boxes; i++) {
-        const $tr = $("<tr>").attr("id", `row${i}`)
+        // const $tr = $("<tr>").attr("id", `row${i}`)
 
         for (let j = 0; j < data.arena.boxes; j++) {
-            const $td = $("<td>").attr("id", `row${i}cell${j}`);
-            $tr.append($td)
+            const $td = $("<div>").attr("id", `row${i}cell${j}`);
+            const gr = i + 1;
+            const gc = j + 1
+            $td.css("grid-row", `${gr}`);
+            $td.css("grid-column", `${gc}`)
+            $td.addClass("cell")
+            $arena.append($td)
         }
-        $table.append($tr)
+        // $table.append($tr)
     }
-    $div.append($table)
-    $(".container").append($div)
+    $table.append($arena)
+    $(".container").append($table)
 }
 
 const makePoints = () => {
@@ -225,33 +220,27 @@ const makePoints = () => {
     $(".container").append($div);
 }
 
-const initGame = (speed) => {
-    makePoints();
-    makeTable();
-    showSnake();
-    makeFood();
-    
-    gameOn = setInterval(showSnake, speed);   
-}
-
 const handleStartEasy = () => {
     $("#rules").remove();
-    $(".controls").remove();
-    data.speed = 250;
-    initGame(250);
+    
+    data.speed = 150;
+    data.displaySpeed = "easy";
+    initGame(data.speed);
 }
 
 const handleStartMedium = () => {
     $("#rules").remove();
-    $(".controls").remove();
-    data.speed = 175;
-    initGame(175);
+    
+    data.speed = 100;
+    data.displaySpeed = "medium";
+    initGame(data.speed);
 }
 const handleStartHard = () => {
     $("#rules").remove();
-    $(".controls").remove();
-    data.speed = 75;
-    initGame(75);
+    
+    data.speed = 50;
+    data.displaySpeed = "hard";
+    initGame(data.speed);
 }
 
 const startingPage = () => {
@@ -278,11 +267,11 @@ const startingPage = () => {
     const $left = $("<div>").attr("id", "left").text("A or ⥢")
     
     $controls.append($up, $down, $left, $right)
-    $(".container").append($controls)
+    $($rules).append($controls)
 };
 
 const main = () => {
-   startingPage();
-}
-
-$(main);
+    startingPage();
+ }
+ 
+ $(main);
